92c6370984e529b2efc3aac5acb6a49332e3bdea
Zuzanna Skokowska <zuzanna.skokowska@icloud.com>
2023-07-13 16:08:43 +0200

    get and post

--- components/Form.js
+++ components/Form.js
@@ -25,15 +25,14 @@ const Label = styled.label`
 `;
 
 export default function Form({ onSubmit, formName, defaultData }) {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-    onSubmit(data);
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target);
+  //   const data = Object.fromEntries(formData);
+  //   onSubmit(data);
 
   return (
-    <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
+    <FormContainer aria-labelledby={formName} onSubmit={onSubmit}>
       <Label htmlFor="name">Name</Label>
       <Input
         id="name"
